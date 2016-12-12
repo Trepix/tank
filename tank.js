e76554681ac634cadf0fa6a7e16c4936a2323ed4
Arnau Sangrà Rocamora <arnsangra@gmail.com>
2016-12-12 14:17:44 +0100

    wip: detecting enemy close to and handling aiming.

--- tank.js
+++ tank.js
@@ -19,6 +19,9 @@ module.exports = function (map) {
 			bottom: {x: 0, y: 1},
 			right: {x: 1, y: 0}
 		},
+        tank = map.you,
+        movement = movements[tank.direction],
+        nextField = { x: tank.x + movement.x, y: tank.y + movement.y },
 		outsideMap = function (point) {
 			return point.x < 0 || point.x >= map.mapWidth || point.y < 0 || point.y >= map.mapHeight;
 		},
@@ -32,10 +35,53 @@ module.exports = function (map) {
 			}
 			return false;
 		},
-		tank = map.you,
-		movement = movements[tank.direction],
-		nextField = { x: tank.x + movement.x, y: tank.y + movement.y };
+		isEnemyReachable = function(curPos, position, range) {
+            var minX = curPos.x - range;
+            var minY = curPos.y - range;
+			var maxX = curPos.x + range;
+			var maxY = curPos.y + range;
+			var isInRange = position.x > minX && position.x <= maxX && position.y >= minY && position.y <= maxY;
+			return isInRange;
+		},
+		amIAimingCorrectly = function (tank1, tank2) {
+			var correct;
+			switch (tank1.direction) {
+				case 'left':
+					correct = tank2.x < tank1.x;
+					break;
+                case 'right':
+                    correct = tank2.x > tank1.x;
+                    break;
+                case 'top':
+                    correct = tank2.y < tank1.y;
+                    break;
+                case 'bottom':
+                    correct = tank2.y > tank1.y;
+                    break;
+			}
+			return correct;
+        },
+        aimToTarget = function (tank1, tank2) {
+			return 'turn-left';
+        };
 
+	for(var i = 0; i < map.enemies.length; ++i ) {
+		if(isEnemyReachable(you, map.enemies[i], map.weaponRange)) {
+			console.log("enemy found within weapon range (you:" + you.x + ',' + you.y + "; tank: " + map.enemies[i].x + ',' +map.enemies[i].y + ")");
+
+			if(!amIAimingCorrectly(you, map.enemies[i])) {
+                console.log("rotating...");
+                var direction = aimToTarget(you, map.enemies[i]);
+				return direction;
+			}
+			else {
+				console.log("aiming correctly");
+				return 'fire';
+			}
+
+
+		}
+	}
 
 	if (utils.isSuddenDeath(map)) {
 		console.log('SUDDEN DEATH!');
